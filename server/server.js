2ea5ffd06e7b5f9141496a4db19af397c09554b2
AlondraCordoba <alondracordoba0720@gmail.com>
2021-03-23 15:48:57 -0600

    Primer Commit de Back End de App Planner

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const hostname = '127.0.0.1';
 
-const morgan = require('morgan');
+//const morgan = require('morgan');
 const app = express();
 
 // parse application/x-www-form-urlencoded
@@ -20,8 +20,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //Middlewares
-app.use(morgan('dev'));
-const opcionesGet = require('./middlewares/opcionesGet');
+//app.use(morgan('dev'));
+//const opcionesGet = require('./middlewares/opcionesGet');
 
 // Habilita CORS
 app.use(cors());
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(opcionesGet);
+//app.use(opcionesGet);
 app.use('/', require('./routes/routes'));
 
 // process.env.URLDB = "mongodb://localhost:27017/appPlanner";
