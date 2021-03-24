8b2bc7f91c193ff59d9fe0018953873c4e515699
AlondraCordoba <alondracordoba0720@gmail.com>
2021-03-24 09:07:02 -0600

    Primer Commit de Back End de App Planner

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const morgan = require('morgan');
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
